4f2a23b41009f7a0544d3b6ceb33e2d45508e491
Elmar Peise <peise@aices.rwth-aachen.de>
2015-10-26 01:26:56 +0100

    randomization and error checking

--- test/util.c
+++ test/util.c
@@ -5,7 +5,7 @@
 #include <math.h>
 
 void s2matgen(int m, int n, float *A, float *B) {
-    srand(time(NULL));
+    srand(time(NULL) + (size_t) A);
     int i, j;
     for (i = 0; i < m; i++)
         for (j = 0; j < n; j++)
@@ -13,7 +13,7 @@ void s2matgen(int m, int n, float *A, float *B) {
 }
 
 void d2matgen(int m, int n, double *A, double *B) {
-    srand(time(NULL));
+    srand(time(NULL) + (size_t) A);
     int i, j;
     for (i = 0; i < m; i++)
         for (j = 0; j < n; j++)
@@ -21,7 +21,7 @@ void d2matgen(int m, int n, double *A, double *B) {
 }
 
 void c2matgen(int m, int n, float *A, float *B) {
-    srand(time(NULL));
+    srand(time(NULL) + (size_t) A);
     int i, j;
     for (i = 0; i < m; i++)
         for (j = 0; j < n; j++) {
@@ -31,7 +31,7 @@ void c2matgen(int m, int n, float *A, float *B) {
 }
 
 void z2matgen(int m, int n, double *A, double *B) {
-    srand(time(NULL));
+    srand(time(NULL) + (size_t) A);
     int i, j;
     for (i = 0; i < m; i++)
         for (j = 0; j < n; j++) {
@@ -43,39 +43,54 @@ void z2matgen(int m, int n, double *A, double *B) {
 int i2vecerr(int n, const int *x, const int *y) {
     int i;
     int error = 0;
-    for (i = 0; i < n; i++)
-        error = MAX(error, abs(x[i] - y[i]));
+    for (i = 0; i < n; i++) {
+        int nom = abs(x[i] - y[i]);
+        int den = abs(y[i]);
+        error = MAX(error, MIN(nom, nom / (float) den));
+    }
     return error;
 }
 
 float s2vecerr(int n, const float *x, const float *y) {
     int i;
     float error = 0;
-    for (i = 0; i < n; i++)
-        error = MAX(error, fabsf(x[i] - y[i]));
+    for (i = 0; i < n; i++) {
+        float nom = fabsf(x[i] - y[i]);
+        float den = fabsf(y[i]);
+        error = MAX(error, MIN(nom, nom / den));
+    }
     return error;
 }
 
 double d2vecerr(int n, const double *x, const double *y) {
     int i;
     double error = 0;
-    for (i = 0; i < n; i++)
-        error = MAX(error, fabs(x[i] - y[i]));
+    for (i = 0; i < n; i++) {
+        double nom = fabs(x[i] - y[i]);
+        double den = fabs(y[i]);
+        error = MAX(error, MIN(nom, nom / den));
+    }
     return error;
 }
 
 float c2vecerr(int n, const float *x, const float *y) {
     int i;
     float error = 0;
-    for (i = 0; i < n; i++)
-        error = MAX(error, sqrtf((x[2 * i] - y[2 * i]) * (x[2 * i] - y[2 * i]) + (x[2 * i + 1] - y[2 * i + 1]) * (x[2 * i + 1] - y[2 * i + 1])));
+    for (i = 0; i < n; i++) {
+        float nom = sqrtf((x[2 * i] - y[2 * i]) * (x[2 * i] - y[2 * i]) + (x[2 * i + 1] - y[2 * i + 1]) * (x[2 * i + 1] - y[2 * i + 1]));
+        float den = sqrtf(y[2 * i] * y[2 * i] + y[2 * i + 1] * y[2 * i + 1]);
+        error = MAX(error, MIN(nom, nom / den));
+    }
     return error;
 }
 
 double z2vecerr(int n, const double *x, const double *y) {
     int i;
     double error = 0;
-    for (i = 0; i < n; i++)
-        error = MAX(error, sqrt((x[2 * i] - y[2 * i]) * (x[2 * i] - y[2 * i]) + (x[2 * i + 1] - y[2 * i + 1]) * (x[2 * i + 1] - y[2 * i + 1])));
+    for (i = 0; i < n; i++) {
+        double nom = sqrt((x[2 * i] - y[2 * i]) * (x[2 * i] - y[2 * i]) + (x[2 * i + 1] - y[2 * i + 1]) * (x[2 * i + 1] - y[2 * i + 1]));
+        double den = sqrt(y[2 * i] * y[2 * i] + y[2 * i + 1] * y[2 * i + 1]);
+        error = MAX(error, MIN(nom, nom / den));
+    }
     return error;
 }
