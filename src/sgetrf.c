6892681bdb10e25ca9ff055d5c921d4acdf8f1e7
Elmar Peise <peise@aices.rwth-aachen.de>
2015-11-04 13:31:30 +0100

    pivot correction fixed

--- src/sgetrf.c
+++ src/sgetrf.c
@@ -67,7 +67,7 @@ void LARPACK(sgetrf)(const int *m, const int *n,
     LAPACK(slaswp)(&n1, A_BL, ldA, iONE, &n2, ipiv_B, iONE);
     // shift pivots
     int i;
-    for (i = 0; i < mn; i++)
+    for (i = 0; i < n2; i++)
         ipiv_B[i] += n1;
 
     if (*n == mn)
